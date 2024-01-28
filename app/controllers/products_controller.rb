22f3bf6e2b383753625ae3f99d61dc96a3abbdb9
sakata007 <be_best72@yahoo.co.jp>
2024-01-28 14:23:59 +0900

    確認画面への遷移実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,32 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  
 
+  def confirm
+    puts "------"
+    puts "confirm"
+    puts "------"
+    # @products = Product.all
+
+    # Turbo Streams を使用せずに通常の HTML レスポンスを返す
+    # respond_to do |format|
+    #   format.html { render "order/confirm" }
+    # end
+    render "confirm"
+  end
+
+  def confirm2
+    render "products/confirm2"
+  end
+
+  # def info
+  #   render "confirm"
+  # end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :email, :date, :address)
+  end
+  
 end
