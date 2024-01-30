daaa98b2e5a141a1d6d449cc261e7d15ec91225a
macos <macos@nakamurakento.AirPort>
2024-01-30 11:33:45 +0900

    base_priceカラムの追加・seedsファイルを英語表記に修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_002951) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_022639) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_002951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "fast_shipping_flg", default: false, null: false
+    t.string "base_price"
   end
 
   create_table "settings", charset: "utf8mb3", force: :cascade do |t|
