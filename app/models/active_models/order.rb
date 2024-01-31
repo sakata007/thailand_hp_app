97e49f39dac43e916d95d05a907a123435704158
sakata007 <be_best72@yahoo.co.jp>
2024-01-31 19:37:17 +0900

    入力した値を確認画面に表示

--- app/models/active_models/order.rb
+++ app/models/active_models/order.rb
@@ -2,7 +2,7 @@ class Order
     include ActiveModel::Model
     include ActiveModel::Attributes
     
-    attr_accessor :name, :address, :date, :email, :phone_number, :commit, :order
+    attr_accessor :name, :address, :date, :email, :phone_number, :commit, :select
 
     # MEMO:オーダー関連のバリデーションはここに記載
 end
