de7483769795144f87ed2209c16d58b56e2c13fa
sakata007 <be_best72@yahoo.co.jp>
2024-02-04 17:53:17 +0900

    管理者/商品一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
   get '/'=> 'orders#index', as: 'top'
   
   # TODO: /admin以下にbasic認証
-  get '/admin/products/create'=> 'products#create', as: 'create_product'
-  post '/admin/products/store'=> 'products#store', as: 'store_product'
+
+  namespace :admin do
+    resources :products
+  end
+  # get '/admin/products/index'=> 'products#index', as: 'index_product'
+  # get '/admin/products/create'=> 'products#create', as: 'create_product'
+  # get '/admin/products/create'=> 'products#create', as: 'create_product'
+  # post '/admin/products/store'=> 'products#store', as: 'store_product'
 
   # メールテストのため追記
   if Rails.env.development?
