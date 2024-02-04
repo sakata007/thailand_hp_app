0921dd15f64bf3d55543b5fd089f6e025237ebde
sakata007 <be_best72@yahoo.co.jp>
2024-02-04 22:55:00 +0900

    編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,9 @@ Rails.application.routes.draw do
   get '/'=> 'orders#index', as: 'top'
   
   # TODO: /admin以下にbasic認証
-
   namespace :admin do
-    resources :products
+    resources :products, expect: [:show]
   end
-  # get '/admin/products/index'=> 'products#index', as: 'index_product'
-  # get '/admin/products/create'=> 'products#create', as: 'create_product'
-  # get '/admin/products/create'=> 'products#create', as: 'create_product'
-  # post '/admin/products/store'=> 'products#store', as: 'store_product'
 
   # メールテストのため追記
   if Rails.env.development?
