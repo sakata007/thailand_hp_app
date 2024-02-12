d4b31437584ff1ac37a461d03eb83e27e085f274
sakata007 <be_best72@yahoo.co.jp>
2024-02-13 01:04:37 +0900

    unicorn設定を追記

---
+++ config/unicorn.rb
@@ -0,0 +1,29 @@
+#｛app_name｝の箇所はご自身のファイルパスを記述してください。
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+ timeout 15
+ preload_app true
+ 
+ listen '/var/www/｛app_name｝/tmp/unicorn.sock'
+ pid    '/var/www/｛app_name｝/tmp/unicorn.pid'
+ 
+ before_fork do |server, worker|
+   Signal.trap 'TERM' do
+     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+     Process.kill 'QUIT', Process.pid
+   end
+ 
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.connection.disconnect!
+ end
+ 
+ after_fork do |server, worker|
+   Signal.trap 'TERM' do
+     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+   end
+ 
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.establish_connection
+ end
+ 
+ stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+ stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
