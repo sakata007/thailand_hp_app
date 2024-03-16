37957e56be82ac86863f731ec0e1c5ab03fc0de0
sakata007 <be_best72@yahoo.co.jp>
2024-03-16 09:42:58 +0900

    puma.rbのbindのパスを変更

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,8 @@
 # state_path "#{app_dir}/tmp/pids/puma.state"
 # stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+bind "unix:///var/www/thailand_hp_app/shared/tmp/sockets/puma.sock;"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 pidfile "#{Rails.root}/tmp/pids/puma.pid"
 state_path "#{Rails.root}/tmp/pids/puma.state"
 stdout_redirect "#{Rails.root}/log/puma.stdout.log", "#{Rails.root}/log/puma.stderr.log", true
